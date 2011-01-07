9c4dda767ba666b7c60fb607e51ccc3ab3463fff
Christopher Durtschi <christopher.durtschi@gmail.com>
2011-01-06 21:42:06 -0700

    Remove unused underscore/camel middleware

--- lib/tropo_rest/connection.rb
+++ lib/tropo_rest/connection.rb
@@ -1,9 +1,7 @@
 require 'faraday'
 require 'faraday/request/multi_json'
-# require 'faraday/request/underscore_to_camel'
 require 'faraday/response/multi_json'
 require 'faraday/response/resource'
-# require 'faraday/response/camel_to_underscore'
 require 'faraday/response/raise_http_errors'
 
 module TropoRest
@@ -17,13 +15,11 @@ module TropoRest
       }
 
       Faraday::Connection.new(options) do |connection|
-        # connection.use Faraday::Request::UnderscoreToCamel
         connection.use Faraday::Request::MultiJson
         connection.adapter(adapter)
         connection.basic_auth(username, password)
         connection.use Faraday::Response::MultiJson
         connection.use Faraday::Response::Resource, resource
-        # connection.use Faraday::Response::CamelToUnderscore
         connection.use Faraday::Response::RaiseHttpErrors
       end
     end
